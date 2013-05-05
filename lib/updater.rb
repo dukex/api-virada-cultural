26d67afc187a7feb3fdf03955f65c3e9079e846c
Duke <duke@vertigem.xxx>
2013-05-05 20:59:33 -0300

    added updater to get stages and save

--- lib/updater.rb
+++ lib/updater.rb
@@ -3,6 +3,12 @@ require 'models'
 
 class Updater
   def self.now
-
+    StageBot.each do |stage_bot|
+      if stage = Stage.get(stage_bot.location_id)
+         stage.update! stage_bot.attributes_to_update
+      else
+        Stage.create! stage_bot.attributes_to_create
+      end
+    end
   end
 end
