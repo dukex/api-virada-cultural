b3dbe8a159771f1caf84bb8dea35fe543a45a0eb
Duke <duke@vertigem.xxx>
2013-05-06 02:54:22 -0300

    updated strings

--- lib/models.rb
+++ lib/models.rb
@@ -1,6 +1,7 @@
 models_path = File.expand_path('../../app/models', __FILE__)
 $:.unshift models_path unless $:.include?(models_path)
 
+DataMapper::Property::String.length(255)
 DataMapper::Logger.new($stdout, :debug)
 DataMapper.setup(:default, ENV['DATABASE_URL']||"sqlite3://#{Dir.pwd}/database.sqlite3")
 
